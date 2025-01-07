c9277bcac59e110230c4ccd27fc3294a2d8fa54a
rahul-dev7x <rahulbera9932@gmail.com>
2025-01-07 17:25:15 +0530

    feat: enhance Dashboard component with user authentication and logout functionality

--- admin/src/layout/Dashboard.jsx
+++ admin/src/layout/Dashboard.jsx
@@ -1,6 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { toast } from 'sonner';
 
 const Dashboard = () => {
+const {user}=useSelector(state=>state.auth);
+const handleLogout=()=>{
+  localStorage.removeItem("adminToken");
+  toast.success("User Logged Out Successfully");
+  navigate("/auth/login")
+}
+const navigate=useNavigate()
+
+  useEffect(()=>{
+
+  },[user])
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       <aside className="w-64 bg-gray-600/90 text-white fixed left-0 h-screen">
@@ -21,8 +36,27 @@ const Dashboard = () => {
         </nav>
       </aside>
       <main className="flex-1  bg-white ml-64">
-        <div className="shadow-md mb-4 p-6">
+        <div className="shadow-md mb-4 p-6 flex justify-between items-center">
           <p className="text-center font-medium text-2xl">Dashboard</p>
+          <div className="flex items-center gap-4">
+            <p className="text-xl font-medium ">{user.name?`Welcome Back ${user.name}`:""}</p>
+            {user ? (
+        <button
+          onClick={handleLogout}
+          className="px-4 py-2 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700 transition-all duration-200"
+        >
+          Logout
+        </button>
+      ) : (
+        <button
+          onClick={() => navigate("/auth/login")}
+          className="px-4 py-2 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 transition-all duration-200"
+        >
+          Login
+        </button>
+      )}
+            
+          </div>
         </div>
 
         <div className="m-4">
